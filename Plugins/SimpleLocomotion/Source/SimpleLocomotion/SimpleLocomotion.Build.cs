01e7f638d4c043fedfc84f278bbfadbe5af506d5
Matt-Makes <15056270085@163.com>
2024-09-06 07:48:56 +0800

    add SimpleLocomotion plugin

---
+++ Plugins/SimpleLocomotion/Source/SimpleLocomotion/SimpleLocomotion.Build.cs
@@ -0,0 +1,29 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class SimpleLocomotion : ModuleRules
+{
+	public SimpleLocomotion(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"GameplayTags",
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"GameplayTags"
+			}
+			);
+	}
+}
