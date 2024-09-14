5411bc5e2379f88a7e5f44c1e59e90e84e29379f
Matt-Makes <15056270085@163.com>
2024-09-14 08:23:58 +0800

    add SimpleAnimInstance class

---
+++ Plugins/SimpleLocomotion/Source/SimpleLocomotion/Private/SimpleAnimInstance.cpp
@@ -0,0 +1,441 @@
+// Copyright (c) Jared Taylor. All Rights Reserved
+
+
+#include "SimpleAnimInstance.h"
+
+#include "SimpleAnimComponent.h"
+#include "SimpleAnimInstanceProxy.h"
+#include "SimpleLocomotionStatics.h"
+#include "SimpleGameplayTags.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(SimpleAnimInstance)
+
+DEFINE_LOG_CATEGORY_STATIC(LogSimpleAnim, Log, All);
+
+namespace SimpleAnimInstanceCVars
+{
+#if WITH_EDITOR
+	static bool bPrintFailedValidationToMessageLog = true;
+	FAutoConsoleVariableRef CVarPrintFailedValidationToMessageLog(
+		TEXT("a.SimpleAnim.PrintFailedValidationToMessageLog"),
+		bPrintFailedValidationToMessageLog,
+		TEXT("Prints failed validation to message log instead of output log."),
+		ECVF_Default);
+#endif
+	
+	static bool bPrintInvalidGameplayTagStates = true;
+	FAutoConsoleVariableRef CVarPrintInvalidGameplayTagStates(
+		TEXT("a.SimpleAnim.PrintInvalidGameplayTagStates"),
+		bPrintInvalidGameplayTagStates,
+		TEXT("Prints invalid gameplay tag states to output log."),
+		ECVF_Default);
+}
+
+
+USimpleAnimInstance::USimpleAnimInstance(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	Gait = FSimpleGameplayTags::Simple_Gait_Run;
+	StartGait = FSimpleGameplayTags::Simple_Gait_Run;
+	StopGait = FSimpleGameplayTags::Simple_Gait_Run;
+	Stance = FSimpleGameplayTags::Simple_Stance_Stand;
+}
+
+FAnimInstanceProxy* USimpleAnimInstance::CreateAnimInstanceProxy()
+{
+	return new FSimpleAnimInstanceProxy(this);
+}
+
+void USimpleAnimInstance::NativeInitializeAnimation()
+{
+	Owner = TryGetPawnOwner();
+	OwnerComponent = Owner ? Owner->GetComponentByClass<USimpleAnimComponent>() : nullptr;
+
+	if (!Owner || !OwnerComponent)
+	{
+		return;
+	}
+
+	// Bind landed delegate
+	if (OwnerComponent)
+	{
+		if (FSimpleLandedSignature* LandedDelegatePtr = OwnerComponent->GetSimpleOnLandedDelegate())
+		{
+			if (LandedDelegatePtr->IsBoundToObject(this))
+			{
+				LandedDelegatePtr->Unbind();
+			}
+			LandedDelegatePtr->BindDynamic(this, &ThisClass::OnLanded);
+		}
+	}
+
+	// Bind cardinal update delegates
+	for (auto& CardinalItr : Cardinals.GetCardinals())
+	{
+		FSimpleCardinal& Cardinal = CardinalItr.Value;
+		if (Cardinal.bEnabled && !Cardinal.UpdateDelegate.IsBoundToObject(this))
+		{
+			Cardinal.UpdateDelegate.BindUObject(this, &ThisClass::UpdateCardinal);
+		}
+	}
+
+	bFirstUpdate = true;
+}
+
+void USimpleAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	if (!IsAnimValidToUpdate(DeltaTime))
+	{
+		return;
+	}
+
+	bWasMovingLastUpdate = !Local2D.Velocity.IsZero();
+
+	LocalRole = OwnerComponent->GetSimpleLocalRole();
+
+	World.Velocity = OwnerComponent->GetSimpleVelocity();
+	World.Acceleration = OwnerComponent->GetSimpleAcceleration();
+	ForwardVector = Owner->GetActorForwardVector();
+	RightVector = Owner->GetActorRightVector();
+
+	PrevWorldRotation = WorldRotation;
+	WorldRotation = Owner->GetActorRotation();
+	ControlRotation = OwnerComponent->GetSimpleControlRotation();
+	BaseAimRotation = OwnerComponent->GetSimpleBaseAimRotation();
+	
+	MaxSpeed = OwnerComponent->GetSimpleMaxSpeed();
+	MaxGaitSpeeds = OwnerComponent->GetSimpleMaxGaitSpeeds();
+	LeanRate = LeanRateOverride >= 0.f ? LeanRateOverride : OwnerComponent->GetSimpleLeanRate();
+
+	RootYawOffset = OwnerComponent->GetSimpleRootYawOffset();
+
+	bIsCurrentFloorWalkable = OwnerComponent->IsSimpleCurrentFloorWalkable();
+	bIsMovingOnGround = OwnerComponent->GetSimpleIsMovingOnGround() && bIsCurrentFloorWalkable;
+	bInAir = OwnerComponent->GetSimpleIsFalling() || !bIsCurrentFloorWalkable;
+	bCanJump = OwnerComponent->GetSimpleCanJump();
+	GravityZ = OwnerComponent->GetSimpleGravityZ();
+	bMovementIs3D = OwnerComponent->GetSimpleMovementIs3D();
+
+	bIsCrouching = OwnerComponent->GetSimpleIsCrouching();
+
+	bIsWalking = OwnerComponent->GetSimpleIsWalking();
+	bIsSprinting = OwnerComponent->GetSimpleIsSprinting();
+
+	bWantsWalking = OwnerComponent->GetSimpleWantsWalking();
+	bWantsSprinting = OwnerComponent->GetSimpleWantsSprinting();
+
+	bWantsLandingFrameLock = OwnerComponent->WantsFrameLockOnLanding();
+	bIsMoveModeValid = OwnerComponent->GetSimpleIsMoveModeValid();
+}
+
+void USimpleAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaTime)
+{
+	if (!IsAnimValidToUpdate(DeltaTime))
+	{
+		return;
+	}
+
+	// Movement properties
+	Local = World.GetLocal(WorldRotation);
+	Local2D = Local.Get2D();
+	World2D = World.Get2D();
+
+	PrevSpeed = Speed;
+	Speed3D = Local.Velocity.Size();
+	Speed2D = Local2D.Velocity.Size();
+	Speed = bIsMovingOnGround ? Speed3D : Speed2D;
+	const float SpeedSq = Speed * Speed;
+
+	const float AccelMag3D = Local.Acceleration.SizeSquared();
+	const float AccelMag2D = Local2D.Acceleration.SizeSquared();
+	const float AccelSq = bMovementIs3D ? AccelMag3D : AccelMag2D;
+	
+	bHasVelocity = !FMath::IsNearlyZero(SpeedSq);
+	bHasAcceleration = !FMath::IsNearlyZero(AccelSq);
+
+	// Rotation properties
+	if (LocalRole != ROLE_SimulatedProxy)
+	{
+		// Control Rotation is more accurate, but BaseAimRotation is replicated to sim proxies
+		BaseAimRotation = ControlRotation;
+	}
+
+	// Update cardinal properties
+	// if (bWantsCardinalsUpdated)
+	{
+		Cardinals.ThreadSafeUpdate(World2D, WorldRotation, RootYawOffset);
+	}
+
+	// Update gait modes
+	NativeThreadSafeUpdateGaitMode(DeltaTime);
+
+	// Update stance
+	NativeThreadSafeUpdateStance(DeltaTime);
+
+	// Extension point
+	NativeThreadSafePostUpdateMovementProperties(DeltaTime);
+
+	if (bFirstUpdate)
+	{
+		// There is no valid delta on the first frame
+		LeanAngle = 0.f;
+	}
+	else if (bWantsLeansUpdated)
+	{
+		const float YawDelta = WorldRotation.Yaw - PrevWorldRotation.Yaw;
+		const float YawDeltaSpeed = YawDelta / DeltaTime;
+		const float ScaledLeanRate = LeanRate / 100.f;  // 3.75 is a friendlier number than 0.0375 for designers
+		LeanAngle = YawDeltaSpeed * ScaledLeanRate;
+	}
+	else
+	{
+		LeanAngle = 0.f;
+	}
+
+	// Extension point
+	NativeThreadSafePreUpdateInAirProperties(DeltaTime);
+	
+	// Landing Frame Lock
+	if (bWantsLandingFrameLock && bIsMoveModeValid)
+	{
+		bLandingFrameLock = true;
+	}
+	
+	if (bLandingFrameLock && bIsMovingOnGround && bCanJump)
+	{
+		// OnLanded doesn't always trigger after mantling
+		bLandingFrameLock = false;
+	}
+
+	// Jumping and falling
+	NativeThreadSafeUpdateFalling(DeltaTime);
+
+	// Extension point
+	NativeThreadSafeUpdateAnimationPreCompletion(DeltaTime);
+
+	bFirstUpdate = false;
+}
+
+void USimpleAnimInstance::NativeThreadSafeUpdateGaitMode(float DeltaTime)
+{
+	// Start Gait Mode: Use the intended mode
+	StartGait = FSimpleGameplayTags::Simple_Gait_Run;
+	if (bWantsSprinting)
+	{
+		// We will probably start sprinting next frame
+		StartGait = FSimpleGameplayTags::Simple_Gait_Sprint;
+	}
+	else if (bWantsWalking)
+	{
+		// We will probably start walking next frame
+		StartGait = FSimpleGameplayTags::Simple_Gait_Walk;
+	}
+
+	// Gait Mode: Use the current mode
+	const FGameplayTag PrevGait = Gait;
+	if (bIsSprinting)
+	{
+		Gait = FSimpleGameplayTags::Simple_Gait_Sprint;
+	}
+	else if (bIsWalking)
+	{
+		Gait = FSimpleGameplayTags::Simple_Gait_Walk;
+	}
+	else
+	{
+		Gait = FSimpleGameplayTags::Simple_Gait_Run;
+	}
+	bGaitChanged = Gait != PrevGait;
+
+	const float MaxSpeedWalk = MaxGaitSpeeds.GetMaxSpeed(FSimpleGameplayTags::Simple_Gait_Walk);
+	const float MaxSpeedRun = MaxGaitSpeeds.GetMaxSpeed(FSimpleGameplayTags::Simple_Gait_Run);
+	
+	// Stop Gait Mode: Use the previous mode
+	if (bHasAcceleration)
+	{
+		StopGait = Gait;
+		if (Gait == FSimpleGameplayTags::Simple_Gait_Walk)
+		{
+			// If walking and needing to stop, default to walk stop, don't need to do anything here
+		}
+		else if (Gait == FSimpleGameplayTags::Simple_Gait_Run)
+		{
+			// If not at run speed, use walking stop
+			if (Speed < MaxSpeedWalk)
+			{
+				// Use walk speed if we haven't even reached our max walk speed yet
+				StopGait = FSimpleGameplayTags::Simple_Gait_Walk;
+			}
+			else if (Speed < MaxSpeedRun)
+			{
+				// Use the gait mode that our speed is closer to
+				if ((Speed - MaxSpeedWalk) < (MaxSpeedRun - Speed))
+				{
+					StopGait = FSimpleGameplayTags::Simple_Gait_Walk;
+				}
+				else
+				{
+					StopGait = FSimpleGameplayTags::Simple_Gait_Run;
+				}
+			}
+		}
+		else if (Gait == FSimpleGameplayTags::Simple_Gait_Sprint)
+		{
+			if (Speed < MaxSpeedRun)
+			{
+				// Use the gait mode that our speed is closer to if we haven't even reached run speed
+				if ((Speed - MaxSpeedWalk) < (MaxSpeedRun - Speed))
+				{
+					StopGait = FSimpleGameplayTags::Simple_Gait_Walk;
+				}
+				else
+				{
+					StopGait = FSimpleGameplayTags::Simple_Gait_Run;
+				}
+			}
+			else
+			{
+				// Sprint always results in a sprint stop, provided we have exceeded run speed
+				StopGait = FSimpleGameplayTags::Simple_Gait_Sprint;
+			}
+		}
+		else if (SimpleAnimInstanceCVars::bPrintInvalidGameplayTagStates)
+		{
+			FSimpleAnimInstanceProxy& Proxy = GetProxyOnAnyThread<FSimpleAnimInstanceProxy>();
+			Proxy.AddPendingMessage(FString::Printf(TEXT("Invalid Gait Mode: %s"), *Gait.ToString()));
+		}
+	}
+}
+
+void USimpleAnimInstance::NativeThreadSafeUpdateStance(float DeltaTime)
+{
+	// Stance
+	const FGameplayTag PrevStance = Stance;
+	if (bIsCrouching)
+	{
+		Stance = FSimpleGameplayTags::Simple_Stance_Crouch;
+	}
+	else
+	{
+		Stance = FSimpleGameplayTags::Simple_Stance_Stand;
+	}
+	bStanceChanged = Stance != PrevStance;
+}
+
+void USimpleAnimInstance::NativeThreadSafeUpdateFalling(float DeltaTime)
+{
+	// Jumping and falling
+	bIsJumping = false;
+	bIsFalling = false;
+	if (bInAir && !bLandingFrameLock)
+	{
+		if (World.Velocity.Z > 0.f)
+		{
+			bIsJumping = true;
+		}
+		else
+		{
+			bIsFalling = true;
+		}
+	}
+
+	TimeToJumpApex = bIsJumping ? -World.Velocity.Z / GravityZ : 0.f;
+}
+
+void USimpleAnimInstance::NativePostEvaluateAnimation()
+{
+	FSimpleAnimInstanceProxy& Proxy = GetProxyOnAnyThread<FSimpleAnimInstanceProxy>();
+	for (auto& PendingLogs : Proxy.GetPendingMessageLogs())
+	{
+		FMessageLog MsgLog { *PendingLogs.Key };
+		MsgLog.Error(FText::FromString(PendingLogs.Value));
+	}
+
+	Proxy.ResetPendingMessageLogs();
+}
+
+void USimpleAnimInstance::OnLanded(const FHitResult& Hit)
+{
+	bLandingFrameLock = true;
+}
+
+float USimpleAnimInstance::GetCardinalDeadZone(const FGameplayTag& CardinalMode) const
+{
+	return 10.f;
+}
+
+void USimpleAnimInstance::UpdateCardinal(const FGameplayTag& CardinalMode, FSimpleCardinal& Cardinal, const FSimpleCardinals& InCardinals)
+{
+	// Consider not updating the properties you don't need to optimize performance!
+	const float DeadZone = GetCardinalDeadZone(CardinalMode);
+
+	Cardinal.Acceleration			= USimpleLocomotionStatics::SelectSimpleCardinalFromAngle(CardinalMode, InCardinals.Acceleration, DeadZone, Cardinal.Acceleration, bWasMovingLastUpdate);
+	Cardinal.AccelerationNoOffset	= USimpleLocomotionStatics::SelectSimpleCardinalFromAngle(CardinalMode, InCardinals.AccelerationNoOffset, DeadZone, Cardinal.AccelerationNoOffset, bWasMovingLastUpdate);
+	Cardinal.Velocity				= USimpleLocomotionStatics::SelectSimpleCardinalFromAngle(CardinalMode, InCardinals.Velocity, DeadZone, Cardinal.Velocity, bWasMovingLastUpdate);
+	Cardinal.VelocityNoOffset		= USimpleLocomotionStatics::SelectSimpleCardinalFromAngle(CardinalMode, InCardinals.VelocityNoOffset, DeadZone, Cardinal.VelocityNoOffset, bWasMovingLastUpdate);
+}
+
+float USimpleAnimInstance::GetLocomotionCardinalAngle(ESimpleCardinalType CardinalType) const
+{
+	return Cardinals.GetDirectionAngle(CardinalType);
+}
+
+bool USimpleAnimInstance::IsAnimValidToUpdate(float DeltaTime) const
+{
+	const bool bValid = IsValid(Owner) && OwnerComponent;
+	const bool bValidDeltaTime = DeltaTime > 1e-6f;
+
+#if WITH_EDITORONLY_DATA
+	if (!bValid && !HasAnyFlags(RF_DefaultSubObject) && GetWorld() && GetWorld()->IsGameWorld())
+	{
+		FString LogError;
+		if (!IsValid(Owner))
+		{
+			LogError += "Owner is not valid";
+		}
+		if (!OwnerComponent)
+		{
+			if (!LogError.IsEmpty()) { LogError += ". "; }
+			LogError += "No SimpleAnimComponent found.";
+		}
+		if (!LogError.IsEmpty())
+		{
+			OnAnimNotValidToUpdate(LogError);
+		}
+	}
+#else
+	if (!bValid)
+	{
+		OnAnimNotValidToUpdate("");
+	}
+#endif
+	
+	return bValid && bValidDeltaTime;
+}
+
+void USimpleAnimInstance::K2_IsAnimValidToUpdate(float DeltaTime, ESimpleIsValidResult& ResultIsValid)
+{
+	const bool bValid = IsAnimValidToUpdate(DeltaTime);
+	ResultIsValid = bValid ? ESimpleIsValidResult::Valid : ESimpleIsValidResult::NotValid;
+}
+
+void USimpleAnimInstance::OnAnimNotValidToUpdate(FString LogError) const
+{
+#if WITH_EDITORONLY_DATA
+	if (!ConsumedDebugMessages.Contains(LogError))  // Don't spam the message log
+	{
+		const_cast<USimpleAnimInstance*>(this)->ConsumedDebugMessages.Add(LogError);
+		LogError = "USimpleAnimInstance: " + LogError;
+		if (IsInGameThread() && SimpleAnimInstanceCVars::bPrintFailedValidationToMessageLog)
+		{
+			FMessageLog Log { "PIE" };
+			Log.Error(FText::FromString(LogError));
+		}
+		else
+		{
+			FSimpleAnimInstanceProxy& Proxy = const_cast<USimpleAnimInstance*>(this)->GetProxyOnAnyThread<FSimpleAnimInstanceProxy>();
+			Proxy.AddPendingMessage(LogError);
+		}
+	}
+#endif
+}
---
+++ Plugins/SimpleLocomotion/Source/SimpleLocomotion/Public/SimpleAnimInstance.h
@@ -0,0 +1,238 @@
+﻿// Copyright (c) Jared Taylor. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SimpleLocomotionTypes.h"
+#include "Animation/AnimInstance.h"
+#include "SimpleAnimInstance.generated.h"
+
+class USimpleAnimComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLELOCOMOTION_API USimpleAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadOnly, Transient, DuplicateTransient, Category=References)
+	AActor* Owner = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Transient, DuplicateTransient, Category=References)
+	USimpleAnimComponent* OwnerComponent = nullptr;
+
+#if WITH_EDITORONLY_DATA
+	/** Assist designers by cleaning out categories they don't need to see */
+	UPROPERTY(EditDefaultsOnly, Category=Editor)
+	TArray<FName> HideEditorCategories = { "RootMotion", "Notifies", "Montage" };
+	
+	UPROPERTY(EditDefaultsOnly, Category=Editor)
+	TArray<FName> ImportantEditorCategories = {};
+#endif
+
+public:
+	/** Required for CardinalMovement to update. If true, NativeThreadSafeUpdateAnimation() calls CardinalMovement.ThreadSafeUpdate() */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Settings)
+	bool bWantsCardinalsUpdated = true;
+
+	/** Required for LeanAngle to update. If true, NativeThreadSafeUpdateAnimation() updates LeanAngle. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Settings)
+	bool bWantsLeansUpdated = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Properties)
+	FSimpleCardinals Cardinals;
+	
+protected:
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	FSimpleMovement World;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	FSimpleMovement World2D;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	FSimpleMovement Local;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	FSimpleMovement Local2D;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	float Speed3D = 0.f;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	float Speed2D = 0.f;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	float Speed = 0.f;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	float PrevSpeed = 0.f;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	FVector ForwardVector = FVector::ForwardVector;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	FVector RightVector = FVector::RightVector;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	FRotator WorldRotation = FRotator::ZeroRotator;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	FRotator PrevWorldRotation = FRotator::ZeroRotator;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	FRotator ControlRotation = FRotator::ZeroRotator;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	FRotator BaseAimRotation = FRotator::ZeroRotator;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	FSimpleGaitSpeed MaxGaitSpeeds = {};
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	float MaxSpeed = 0.f;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	float LeanRate = 0.f;
+
+	/** If set, will use this instead of LeanRate */
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadWrite, Category=Properties)
+	float LeanRateOverride = -1.f;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	float LeanAngle = 0.f;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	float RootYawOffset = 0.f;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	float GravityZ = 1.f;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	float TimeToJumpApex = 0.f;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	bool bIsCurrentFloorWalkable = true;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	bool bIsMovingOnGround = true;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	bool bInAir = false;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	bool bIsFalling = false;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	bool bIsJumping = false;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	bool bCanJump = false;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	bool bMovementIs3D = false;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	bool bIsCrouching = false;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	bool bIsWalking = false;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	bool bIsSprinting = false;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	bool bWantsWalking = false;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	bool bWantsSprinting = false;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=Properties)
+	bool bIsMoveModeValid = false;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=State)
+	bool bHasAcceleration = false;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=State)
+	bool bHasVelocity = false;
+
+public:
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=State)
+	FGameplayTag Gait;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=State)
+	FGameplayTag StartGait;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=State)
+	FGameplayTag StopGait;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=State)
+	FGameplayTag Stance;
+
+protected:
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=State)
+	bool bStanceChanged = false;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=State)
+	bool bGaitChanged = false;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=State)
+	TEnumAsByte<ENetRole> LocalRole = ROLE_AutonomousProxy;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=System)
+	bool bFirstUpdate = true;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=System)
+	bool bWasMovingLastUpdate = false;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=System)
+	bool bWantsLandingFrameLock = false;
+	
+	/** This prevents poor blending with systems such as mantling */
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category=System)
+	bool bLandingFrameLock = false;
+	
+public:
+	USimpleAnimInstance(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	
+	virtual FAnimInstanceProxy* CreateAnimInstanceProxy() override;
+
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaTime) override;
+
+	virtual void NativeThreadSafeUpdateGaitMode(float DeltaTime);
+	virtual void NativeThreadSafeUpdateStance(float DeltaTime);
+	virtual void NativeThreadSafePostUpdateMovementProperties(float DeltaTime) {}
+	virtual void NativeThreadSafeUpdateFalling(float DeltaTime);
+	virtual void NativeThreadSafePreUpdateInAirProperties(float DeltaTime) {}
+	virtual void NativeThreadSafeUpdateAnimationPreCompletion(float DeltaTime) {}
+
+	virtual void NativePostEvaluateAnimation() override;
+
+protected:
+	UFUNCTION()
+	virtual void OnLanded(const FHitResult& Hit);
+	
+	virtual float GetCardinalDeadZone(const FGameplayTag& CardinalMode) const;
+
+	void UpdateCardinal(const FGameplayTag& CardinalMode, FSimpleCardinal& Cardinal, const FSimpleCardinals& InCardinals);
+	
+	UFUNCTION(BlueprintPure, Category=Animation, meta=(BlueprintThreadSafe))
+	float GetLocomotionCardinalAngle(ESimpleCardinalType CardinalType) const;
+
+	UFUNCTION(BlueprintPure, Category=Animation, meta=(BlueprintThreadSafe))
+	virtual bool IsAnimValidToUpdate(float DeltaTime) const;
+	
+	UFUNCTION(BlueprintCallable, Category=Animation, meta=(BlueprintThreadSafe, ExpandEnumAsExecs="ResultIsValid", DisplayName="Is Anim Valid to Update"))
+	virtual void K2_IsAnimValidToUpdate(float DeltaTime, ESimpleIsValidResult& ResultIsValid);
+
+	virtual void OnAnimNotValidToUpdate(FString LogError) const;
+	
+#if WITH_EDITORONLY_DATA
+	UPROPERTY(Transient, DuplicateTransient)
+	TArray<FString> ConsumedDebugMessages;
+#endif
+};
